087dfad9749a1b1c52980d331db3f807b2f7f87f
CaioP <caiopatriotabarros@gmail.com>
2023-07-03 22:59:11 -0300

    pequenas edições (alura)

--- src/Filme.java
+++ src/Filme.java
@@ -2,7 +2,22 @@ public class Filme {
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double avalicao;
+    double somaDasAvaliacoes;
     int totalDeAvaliacoes;
     int duracaoEmMinutos;
+
+    void exibeFichaTecnica(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Incluído no plano: " + incluidoNoPlano);
+    }
+
+    void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
 }
